eb906a35460fad4a2c9ec8dfc6f08438ca4d2406
Galindo97 <rexogamer17@gmail.com>
2025-12-18 23:09:59 -0600

    chore: update Svelte adapter from static to Vercel for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel'; // <--- Cambio aquí
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
-		}),
-
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter() // No necesitas configurar carpetas manualmente
+    }
 };
 
 export default config;
